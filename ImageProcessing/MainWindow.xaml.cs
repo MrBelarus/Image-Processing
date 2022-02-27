a83d063034af6586e9d5a55f6bdbccb65e7b333e
Vladislav <lakey38015@gmail.com>
2022-02-27 23:54:30 +0300

    Add project files.

---
+++ ImageProcessing/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using ImageProcessing.Utils;
+using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ImageProcessing {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void OnBtnPerformClick(object sender, RoutedEventArgs e) {
+            
+        }
+
+        #region MenuBarActions
+        private void OnMenuCreateNewClick(object sender, RoutedEventArgs e) {
+            workSpaceImage_original.Source = null;
+            workSpaceImage.Source = null;
+        }
+
+        private void OnMenuOpenFileClick(object sender, RoutedEventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true) {
+                BitmapImage image = ImageManager.LoadImage(openFileDialog.FileName);
+                if (image == null) {
+                    MessageBox.Show("Failed to load image", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                workSpaceImage_original.Source = image;
+
+                //refresh width/height of canvas
+                imageCanvas_original.Width = image.Width;
+                imageCanvas_original.Height = image.Height;
+            }
+        }
+
+        private void OnMenuSaveFileClick(object sender, RoutedEventArgs e) {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true) {
+                try {
+                    ImageManager.SaveImage(workSpaceImage_original, saveFileDialog.FileName, new BmpBitmapEncoder());
+                }
+                catch (Exception ex) {
+                    MessageBox.Show($"{ex.Message}\n Failed to save image", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+        }
+
+        private void OnMenuExitClick(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+        #endregion
+    }
+}
---
+++ ImageProcessing/Utils/ImageManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ImageProcessing.Utils {
+    class ImageManager {
+        public static BitmapImage LoadImage(string path) {
+            try {
+                return new BitmapImage(new Uri(path));
+            }
+            catch {
+                return null;
+            }
+        }
+
+        public static void SaveImage(Image image, string path, BitmapEncoder encoder = null) {
+            if (encoder == null) {
+                encoder = new PngBitmapEncoder();
+            }
+            encoder.Frames.Add(BitmapFrame.Create((BitmapSource)image.Source));
+            using (FileStream stream = new FileStream(path, FileMode.Create)) {
+                encoder.Save(stream);
+            }
+        }
+    }
+}
