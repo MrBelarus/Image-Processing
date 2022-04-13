819c43811c68cd8d403ad8ed17a2df6faf81869a
Vladislav <lakey38015@gmail.com>
2022-04-13 15:29:50 +0300

    <><>

--- ImageProcessing/Code/Core/GridDisplayer.cs
+++ ImageProcessing/Code/Core/GridDisplayer.cs
@@ -6,7 +6,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 
-namespace ImageProcessing.Code.Core {
+namespace ImageProcessing.Core {
     public class GridDisplayer {
         private DataGrid imageMatrix;
 
--- ImageProcessing/MainWindow.xaml.cs
+++ ImageProcessing/MainWindow.xaml.cs
@@ -6,7 +6,6 @@ using System;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using System.Collections.Generic;
-using ImageProcessing.Code.Core;
 
 namespace ImageProcessing {
     /// <summary>
