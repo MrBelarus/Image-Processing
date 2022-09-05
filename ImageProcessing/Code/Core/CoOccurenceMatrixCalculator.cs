f2da399ecc1aa512c7c610267674e86282619615
Vladislav <lakey38015@gmail.com>
2022-09-05 16:56:01 +0300

    CoOccurenceMatrix calculator class

---
+++ ImageProcessing/Code/Core/CoOccurenceMatrixCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProcessing.Core {
+    class CoOccurenceMatrixCalculator {
+        const int dimension = 256;
+        public static int[] GetImageMatrix_C90_D1(ImageData imageData) {
+            int width = imageData.Width, height = imageData.Height;
+
+            Processers.ConvertRGB_ToGreyMax converter = new Processers.ConvertRGB_ToGreyMax();
+            imageData = converter.Process(imageData);
+
+            int[] pixels = imageData.GetPixels();
+
+            int[] result = new int[dimension * dimension];
+
+            for (int x = 0; x < width; x++) {
+                for (int y = 1; y < height; y++) {
+                    int pxl1 = pixels[y * width + x];
+                    int pxl2 = pixels[(y - 1) * width + x];
+
+                    result[pxl1 * dimension + pxl2]++;
+                }
+            }
+
+            return result;
+        }
+
+        public static float[] GetNormalizedMatrix(int[] matrix) {
+            float[] result = new float[matrix.Length];
+
+            int sum = 0;
+            foreach(var val in matrix) {
+                sum += val;
+            }
+
+            for(int i = 0; i < matrix.Length; i++) {
+                result[i] = ((float)matrix[i]) / sum;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ ImageProcessing/Code/Utils/Extentions.cs
@@ -0,0 +1,50 @@
+﻿using MathLab.Entities;
+using System;
+using System.Text;
+
+namespace Lab_4
+{
+    public static class FloatArrayExtention
+    {
+        public static float GetMax(this float[] array)
+        {
+            float result = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > result)
+                    result = array[i];
+            }
+            return result;
+        }
+
+        public static float[] Subtract(this float[] array, float[] with)
+        {
+            float[] result = (float[])array.Clone();
+            for(int i = 0; i < array.Length; i++)
+            {
+                result[i] -= with[i];
+            }
+            return result;
+        }
+
+        public static float[] PasteValues(this float[] array, float[] from)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = from[i];
+            }
+
+            return array;
+        }
+
+        public static float[] Abs(this float[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Math.Abs(array[i]);
+            }
+
+            return array;
+        }
+    }
+}
